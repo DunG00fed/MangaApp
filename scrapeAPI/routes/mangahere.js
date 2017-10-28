64ef907088c91028886fc1a2e5e9be9f0af7d384
Jared <jaredpay@buffalo.edu>
2017-10-28 17:47:00 -0400

    Added scraper API

---
+++ scrapeAPI/routes/mangahere.js
@@ -0,0 +1,136 @@
+//Dependencies
+var express = require('express'),
+  request = require('request'),
+  cheerio = require('cheerio');
+
+var router = express.Router();
+
+const MANGA_HERE_URL = 'http://www.mangahere.co';
+
+
+//Routes
+router.get('/popular', function(req, res) {
+  var page_num = req.param('page');
+
+  console.log("fetching Manga");
+
+  request(MANGA_HERE_URL + '/directory/' + page_num +'.htm', function(err, resp, html) {
+    if (!err && resp.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var manga_list = [];
+
+      $('li', '.directory_list').each(function() {
+
+        manga_list.push({
+          title: $(this).find('.title').children('a').attr('title'),
+          url: $(this).find('.title').children('a').attr('href'),
+          img: $(this).find('img').attr('src'),
+          genre: $(this).find('.title').next().next().text()
+        });
+      });
+      res.send(JSON.stringify(manga_list));
+    }
+  });
+});
+
+router.get('/search', function(req, res) {
+  var genre = req.param('genre');
+  var next_manga = req.param('next');
+
+});
+
+
+router.get('/details', function(req, res) {
+  var manga_url = req.param('url');
+
+  request(manga_url, function(err, resp, html) {
+    if (!err && resp.statusCode == 200) {
+      var $ = cheerio.load(html);
+
+      var manga_info = {};
+      var genre_list = [];
+      var chapter_list = [];
+
+      manga_info.img = ($('.manga_detail_top').children('img').attr('src'));
+      manga_info.title = $('h2').text().replace(' Manga', '');
+      manga_info.summary = $('p#show').text().replace('Show less', '');
+
+      manga_info.genre = $('li.posR').next().next().text().replace('Genre(s):', '');
+
+      $('li', '.detail_list').each(function() {
+        var chapter;
+        chapter = {
+          number: parseFloat($(this).find('a').text().replace(manga_info.title, '').replace('\n', '')),
+          // title: $(this).find('span.left').text(),
+          url: $(this).find('a').attr('href'),
+          date: $(this).find('span.right').text()
+        };
+        chapter_list.push(chapter);
+      });
+
+      manga_info.chapter_list = chapter_list;
+      manga_info.chapter_length = chapter_list.length;
+
+      console.log(manga_info);
+
+      res.send(JSON.stringify(manga_info));
+    }
+  });
+
+});
+
+
+
+router.get('/read', function(req, res) {
+  var chapter_url = req.param('url');
+
+  console.log("fetching Pages");
+
+  console.log(chapter_url);
+
+  request(chapter_url + '1.html', function(err, resp, html) {
+    if (!err && resp.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var page_num = $('.readpage_top').find('select.wid60').children('option').length;
+
+      var pages = [];
+      var promises = [];
+      var count;
+
+      for (count = 1; count <= page_num; count++) {
+        promises.push(getImage(chapter_url + count + '.html'));
+      }
+
+      Promise.all(promises).then(function(values) {
+        count = 0;
+        values.forEach(function() {
+          var new_page = {
+            url: values[count]
+          };
+          pages[count++] = new_page;
+        });
+        console.log(pages);
+        res.send(JSON.stringify(pages));
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }
+  });
+});
+
+function getImage(uri) {
+  var deferred = Promise.defer();
+  request(uri, function(err, res, html) {
+    if (!err && res.statusCode == 200) {
+      var $ = cheerio.load(html);
+      deferred.resolve($('section#viewer').children('a').children('img').attr('src'));
+    } else {
+      deferred.reject(err);
+    }
+  });
+
+  return deferred.promise;
+}
+
+//Return router
+module.exports = router;
