81f9348466705e10b2bed03f832d66a4c0f860ae
Jared <jaredpay@buffalo.edu>
2017-11-06 22:39:15 -0500

    Finished MangaOnline scraper

--- scrapeAPI/routes/mangaonline.js
+++ scrapeAPI/routes/mangaonline.js
@@ -1,7 +1,7 @@
 //Dependencies
 var express = require('express'),
-    request = require('request'),
-    cheerio = require('cheerio');
+  request = require('request'),
+  cheerio = require('cheerio');
 
 var router = express.Router();
 
@@ -29,38 +29,15 @@ router.get('/:manga', function(req, res) {
       var $ = cheerio.load(html);
 
       var manga_info = {};
-      var genre_list = [];
-      var chapter_list = [];
 
-      manga_info.img = ($('#mangaimg').children('img').attr('src'));
-      manga_info.title = $('.aname', 'div#mangaproperties').text();
-      manga_info.summary = $('#readmangasum').children('p').text();
-
-      $('.genretags').each(function() {
-        genre_list.push($(this).text());
-      });
-
-      manga_info.genre = genre_list;
-
-      $('div.chico_manga', '#chapterlist').each(function() {
-        var chapter;
-        chapter = {
-          number: parseFloat($(this).next().text().replace(manga_info.title, '')),
-          title: $(this).parent().text().replace('\n\n' + $(this).next().text() + ' : ', ''),
-          url: $(this).next().attr('href'),
-          date: $(this).parent().next().text()
-        };
-        chapter_list.push(chapter);
-      });
-
-      manga_info.chapter_list = chapter_list;
-      manga_info.chapter_length = chapter_list.length;
+      manga_info.img = $('img.cvr').attr('src');
+      manga_info.title = $('h1.ttl').text();
+      manga_info.chapters = $('ul.chp_lst').find('li').length;
 
       console.log(manga_info);
 
       res.send(JSON.stringify(manga_info));
-    }
-    else {
+    } else {
       console.log(err);
     }
   });
@@ -75,43 +52,65 @@ router.get('/:manga', function(req, res) {
  */
 router.get('/:manga/:chapter', function(req, res) {
   var manga = req.params.manga;
-  var chapter = req.params.chapter;
-  var chapter_url = MANGA_ONLINE_URL + manga + '/' + chapter;
+  var chapter_num = req.params.chapter;
+  var chapter_url = MANGA_ONLINE_URL + manga + '/' + chapter_num;
 
   console.log("fetching Pages");
 
-  request(chapter_url, function(err, resp, html) {
-    if (!err && resp.statusCode == 200) {
-      var $ = cheerio.load(html);
-      var page_num = $('.cbo_wpm_pag').find('option').length;
-
-      var pages = [];
-      var promises = [];
-      var count;
-
-      for (count = 1; count <= page_num; count++) {
-        promises.push(getImage(chapter_url + '/' + count));
-      }
-
-      Promise.all(promises).then(function(values) {
-        count = 0;
-        values.forEach(function() {
-          var new_page = {
-            url: values[count]
-          };
-          pages[count++] = new_page;
-        });
-        console.log(pages);
-        res.send(JSON.stringify(pages));
-      }).catch(function(err) {
-        console.log(err);
-      });
-    }
+  getChapter(chapter_url, chapter_num).then(function(pages) {
+    var chapter = {
+        chapter: chapter_num,
+        pages: pages
+    };
+    console.log(chapter);
+    res.send(JSON.stringify(chapter));
+  }).catch(function(err) {
+    console.log(err);
   });
 });
 
+
 /**
- * Retrieve manga chapter image url.
+ * Retrieve manga chapter image urls.
+ * @method
+ * @param  {string} chapter_url
+ * @param  {integer} chapter_num
+ * @return {promise}
+ */
+function getChapter(chapter_url, chapter_num) {
+  return new Promise(function(resolve, reject) {
+    request(chapter_url, function(err, resp, html) {
+      if (!err && resp.statusCode == 200) {
+        var $ = cheerio.load(html);
+        var page_num = $('.cbo_wpm_pag').find('option').length;
+
+        var pages = [];
+        var promises = [];
+        var count;
+
+        for (count = 1; count <= page_num; count++) {
+          promises.push(getImage(chapter_url + '/' + count));
+        }
+
+        Promise.all(promises).then(function(img_urls) {
+          for(count = 0; count < img_urls.length; count++){
+            pages.push( {
+              page: count + 1,
+              url: img_urls[count]
+            });
+          }
+          // console.log(pages);
+          resolve(pages);
+        }).catch(function(err) {
+          reject(err);
+        });
+      }
+    });
+  });
+}
+
+/**
+ * Retrieve manga page image url.
  * @method
  * @param  {string} uri
  * @return {Promise}
