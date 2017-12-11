32294eb7de891f5ac20996840859e55eee22c735
Jared <jaredpay@buffalo.edu>
2017-12-10 21:26:38 -0500

    Changed Controller method name.

--- server/backend/app/Controllers/Http/RegisterController.js
+++ server/backend/app/Controllers/Http/RegisterController.js
@@ -1,6 +1,9 @@
 'use strict'
 
 class RegisterController {
+  async registerUser () {
+    
+  }
 }
 
 module.exports = RegisterController
--- server/backend/start/routes.js
+++ server/backend/start/routes.js
@@ -19,7 +19,7 @@ Route.group(() => {
 
   Route.post('login', 'AuthController.login')
   Route.post('logout', 'AuthController.logout')
-  Route.post('register', 'RegisterController.register')
+  Route.post('register', 'RegisterController.registerUser')
 
   Route.group(() => {
     Route.resource('users', 'UserController').apiOnly()
