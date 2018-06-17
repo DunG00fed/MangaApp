4eae788e56e1634e1c20ecc7a5382ce68d8d1684
Jared Payne <jpayne3788@gmail.com>
2018-06-16 20:03:33 -0400

    Initialized GraphQL server. I will be replacing the AdnoisJS server in favor of this new Express server.

---
+++ backend/graphqlserver/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
